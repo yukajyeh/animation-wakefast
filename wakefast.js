561e99c2f66cbc865e566eb1d1006a6815f9f4bc
Yuka Yeh <yukaka210@gmail.com>
2021-06-22 17:44:32 +0200

    working on intersection observer

--- wakefast.js
+++ wakefast.js
@@ -1,27 +1,51 @@
-const mainTag = document.querySelector("main")
-const bodyTag = document.querySelector("body")
+const mainTag = document.querySelector("main");
+const bodyTag = document.querySelector("body");
+const figcaptions = document.querySelectorAll("figcaption");
 
-mainTag.style.position = "fixed"
-mainTag.style.top = "0px"
-mainTag.style.left = "0px"
-mainTag.style.width = "100%"
+const motion = window.matchMedia("(perfers-reduced-motion: no-preference)");
+const large = window.matchMedia("(min-width:600px)");
 
+if (motion.matches && large.matches){
 
-let currentScroll = 0
-
-const changeScroll = function () {
-
-    bodyTag.style.height = mainTag.offsetHeight + "px"
+    mainTag.style.position = "fixed";
+    mainTag.style.top = "0px";
+    mainTag.style.left = "0px";
+    mainTag.style.width = "100%";
     
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if(entry.intersectionRatio > 0.25){
+                entry.target.classList.add("in-view");
+            } 
+        });
+    },{
+        threshold: [0, 0.25, 0.5, 0.75, 1]
+    });
 
-    mainTag.style.top = (-1 * currentScroll) + "px"
+    figcaptions.forEach(caption => {
+        observer.observe(caption);
+    });
+    
+    let currentScroll = 0;
+    let aimScroll = 0;
+    
+    const changeScroll = function () {
+    
+        bodyTag.style.height = mainTag.offsetHeight + "px";
+        
+        currentScroll = currentScroll + ( aimScroll - currentScroll) * 0.05;
+    
+        mainTag.style.top = (-1 * currentScroll) + "px";
+    
+        requestAnimationFrame(changeScroll);
+    };
+    
+    window.addEventListener("scroll", function () {
+        aimScroll = window.pageYOffset;
+    });
+    
+    changeScroll();
+    
+};
 
-    requestAnimationFrame(changeScroll)
-}
-
-window.addEventListener("scroll", function () {
-    currentScroll = window.pageYOffset
-})
-
-changeScroll()
 
