d22d4ed51d279d3e79191d3d4f329affb148b9b0
Yuka Yeh <yukaka210@gmail.com>
2021-06-23 14:34:00 +0200

    clip path nv bar

--- wakefast.js
+++ wakefast.js
@@ -2,6 +2,9 @@ const mainTag = document.querySelector("main");
 const bodyTag = document.querySelector("body");
 const figcaptions = document.querySelectorAll("figcaption");
 
+const cursor = document.querySelector("div.cursor");
+const cursorOuter = document.querySelector("div.cursor-outer")
+
 const motion = window.matchMedia("(prefers-reduced-motion: no-preference)");
 const large = window.matchMedia("(min-width:600px)");
 
@@ -50,7 +53,7 @@ if (motion.matches && large.matches){
                 image.style.top = (diff * speed) + "px";
             })
         });
-        
+
         
     
         requestAnimationFrame(changeScroll);
@@ -61,6 +64,37 @@ if (motion.matches && large.matches){
     });
     
     changeScroll();
+
+    let cursorCurrentX = 0;
+    let cursorCurrentY = 0;
+    let cursorOuterCurrentX = 0;
+    let cursorOuterCurrentY = 0;
+    let cursorAimX = 0;
+    let cursorAimY = 0;
+    
+    const changeCursor = function() {
+
+        cursorCurrentX = cursorCurrentX + (cursorAimX - cursorCurrentX) * 0.1;
+        cursorCurrentY = cursorCurrentY + (cursorAimY - cursorCurrentY) * 0.1;
+
+        cursor.style.left = cursorCurrentX + "px";
+        cursor.style.top = cursorCurrentY + "px";
+
+        cursorOuterCurrentX = cursorOuterCurrentX + (cursorAimX - cursorOuterCurrentX) * 0.05;
+        cursorOuterCurrentY = cursorOuterCurrentY + (cursorAimY - cursorOuterCurrentY) * 0.05;
+
+        cursorOuter.style.left = cursorOuterCurrentX + "px";
+        cursorOuter.style.top = cursorOuterCurrentY + "px";
+
+        requestAnimationFrame(changeCursor);
+    };
+
+    document.addEventListener("mousemove", function(event) {
+        cursorAimX = event.pageX;
+        cursorAimY = event.pageY;
+    })
+    
+    changeCursor();
     
 };
 
