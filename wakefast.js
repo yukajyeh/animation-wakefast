4ceba0b2c3ba4c286c8f071486725b5c94788467
Yuka Yeh <yukaka210@gmail.com>
2021-06-23 11:22:43 +0200

    parallex effect

--- wakefast.js
+++ wakefast.js
@@ -2,7 +2,7 @@ const mainTag = document.querySelector("main");
 const bodyTag = document.querySelector("body");
 const figcaptions = document.querySelectorAll("figcaption");
 
-const motion = window.matchMedia("(perfers-reduced-motion: no-preference)");
+const motion = window.matchMedia("(prefers-reduced-motion: no-preference)");
 const large = window.matchMedia("(min-width:600px)");
 
 if (motion.matches && large.matches){
@@ -14,12 +14,12 @@ if (motion.matches && large.matches){
     
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            if(entry.intersectionRatio > 0.25){
+            if(entry.intersectionRatio > 0.5){
                 entry.target.classList.add("in-view");
             } 
         });
     },{
-        threshold: [0, 0.25, 0.5, 0.75, 1]
+        threshold: [0, 0.25, 1]
     });
 
     figcaptions.forEach(caption => {
@@ -36,6 +36,22 @@ if (motion.matches && large.matches){
         currentScroll = currentScroll + ( aimScroll - currentScroll) * 0.05;
     
         mainTag.style.top = (-1 * currentScroll) + "px";
+
+        figcaptions.forEach(caption => {
+            const box = caption.getBoundingClientRect();
+            const midY = box.y + box.height / 2;
+            const midScreen = window.innerHeight / 2;
+            const diff = midY - midScreen ;
+
+            const images = caption.querySelectorAll("img");
+
+            images.forEach((image, index) => {
+                const speed = 0.1 + 0.1 * index;
+                image.style.top = (diff * speed) + "px";
+            })
+        });
+        
+        
     
         requestAnimationFrame(changeScroll);
     };
